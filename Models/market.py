6c5acf3afa3f7e837eb9c9e2962559ab262e860b
Symantas <symantasstatkevicius@gmail.com>
2026-04-12 16:13:14 +0300

    Market class with constructo to finish validation

---
+++ Models/market.py
@@ -0,0 +1,15 @@
+#class Market:
+    #SHOCK_PROBABILITY = 0.01
+    #SHOCK_MULTIPLIER = 5
+    #PRICE_FLOOR = 0.01
+    #def __init__(self,assets):
+        #if isinstance(assets,dict) and len(assets)>0:
+            #self._assets = assets
+            #self._tick_count = 0
+        #else:
+            #raise ValueError("Assets must be a non-empty dictionary check the assets!")
+        
+    #@property
+    #def assets(self):
+        #return self._assets
+        
